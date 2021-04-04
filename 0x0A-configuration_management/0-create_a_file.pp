cf5a21326070e5624ab9f15dc946f3a5ec07559c
ChloeDumit <2106@holbertonschool.com>
2021-04-04 13:59:15 +0000

    puppetlint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-
-file { "/tmp/holberton":
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
-    content => 'I love Puppet'
+# create a file
+file { '/tmp/holberton':
+  path    => '/tmp/holberton',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
 }
