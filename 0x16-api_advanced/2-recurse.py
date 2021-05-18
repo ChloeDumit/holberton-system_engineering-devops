f98ca0bb3b9ff9454634909cc193e245948971b5
ChloeDumit <2106@holbertonschool.com>
2021-05-18 19:06:44 +0000

    all fixed

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-""" Program that use Reddit API and query How many subs are there? and list the
-top ten with recursion"""
+""" Reddit api"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=''):
-    """ ecursive function that queries the Reddit API and returns a list
-    containing the titles of all hot articles for a given subreddit"""
+    """ recurse api"""
     url_hot = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit, after)
     h = {'User-Agent': 'My User Agent'}
     req = requests.get(url_hot, headers=h, allow_redirects=False)
